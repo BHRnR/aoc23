acead72281daf0d4645b321bc5609821602f03f4
Hacker, Benjamin <benjamin.hacker@adesso.de>
2023-12-13 17:03:36 +0100

    Solve 13B

--- src/main/kotlin/13.kt
+++ src/main/kotlin/13.kt
@@ -1,20 +1,53 @@
 import java.io.File
+import java.lang.IllegalStateException
 import kotlin.math.min
 
-fun thirteenth(): Int {
-    return parseInput().sumOf { getMapValue(it) }
+enum class Alignment { HORIZONTAL, VERTICAL }
+
+fun thirteenth() = parseInput()
+//        .map { getMirrorPosition(it) } //Part 1
+    .map { getAlternativeWithFlipped(it) } //Part 2
+    .sumOf { getSum(it) }
+
+
+private fun getMirrorPosition(map: List<String>): Pair<Int, Alignment> {
+    val positions = getMirrorPositions(map)
+    check(positions.size == 1)
+    return positions[0]
 }
 
-private fun getMapValue(map: List<String>): Int {
+private fun getMirrorPositions(map: List<String>): List<Pair<Int, Alignment>> {
+    val result = mutableListOf<Pair<Int, Alignment>>()
     val transposed = transposeData(map)
 
-    val horizontalSum = (0..<map.size - 1)
-        .sumOf { line -> if (isHorizontalMirror(map, line)) (line + 1) * 100 else 0 }
+    for (line in 0..map.size - 2)
+        if (isHorizontalMirror(map, line))
+            result.add(Pair(line, Alignment.HORIZONTAL))
 
-    val verticalSum = (0..<transposed.size - 1)
-        .sumOf { line -> if (isHorizontalMirror(transposed, line)) (line + 1) else 0 }
+    for (col in 0..transposed.size - 2)
+        if (isHorizontalMirror(transposed, col))
+            result.add(Pair(col, Alignment.VERTICAL))
 
-    return horizontalSum + verticalSum
+    return result
+}
+
+private fun getAlternativeWithFlipped(map: List<String>): Pair<Int, Alignment> {
+    val oldPosition = getMirrorPosition(map)
+    for (line in map.indices)
+        for (col in map[line].indices) {
+            val mapWithFlipped = getFlippedAt(map, line, col)
+            val newPositions = getMirrorPositions(mapWithFlipped).toMutableList()
+
+            if (newPositions.any { it != oldPosition })
+                return newPositions.first { it != oldPosition }
+        }
+    throw IllegalStateException("Now new mirror position found")
+}
+
+private fun getFlippedAt(map: List<String>, line: Int, col: Int): List<String> {
+    val result = map.map { it.toMutableList() }
+    result[line][col] = if (map[line][col] == '#') '.' else '#'
+    return result.map { String(it.toCharArray()) }
 }
 
 private fun isHorizontalMirror(map: List<String>, position: Int) =
@@ -28,9 +61,9 @@ private fun parseInput(): List<List<String>> {
 
     var map: MutableList<String> = mutableListOf()
     for (line in input)
-        if (line.isNotEmpty())
+        if (line.isNotEmpty()) {
             map.add(line)
-        else {
+        } else {
             maps.add(map)
             map = mutableListOf()
         }
@@ -41,5 +74,7 @@ private fun parseInput(): List<List<String>> {
 private fun transposeData(map: List<String>) =
     map[0].indices.map { col -> map.map { it[col] }.joinToString("") }
 
-
-
+private fun getSum(position: Pair<Int, Alignment>) =
+    if (position.second == Alignment.HORIZONTAL)
+        ((position.first + 1) * 100)
+    else position.first + 1
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,6 @@
 fun main() {
+    val start = java.util.Date().time
     println(thirteenth())
+    val end = java.util.Date().time
+    println("Took ${end - start}ms.")
 }
