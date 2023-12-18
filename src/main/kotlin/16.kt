22e83e694d2bbfd3c0c483e87e8e13033387e354
Hacker, Benjamin <benjamin.hacker@adesso.de>
2023-12-18 11:44:30 +0100

    Solve 16B

--- src/main/kotlin/16.kt
+++ src/main/kotlin/16.kt
@@ -5,8 +5,25 @@ const val ERROR = "No valid Symbol"
 
 fun sixteenth(): Int {
     val input = File("src/main/resources/input16").readLines().map { it.toList() }
+//    return getEnergizedForStartingPosition(input, Pair(Pair(0, 0), Direction.West)) // Part 1
+    return partB(input) // Part 2
+}
+
+private fun partB(input: List<List<Char>>): Int {
+    var max = 0
+    val startingPositions = getStartingPositions(input)
+    for ((counter, startingPosition) in startingPositions.withIndex()) {
+        println("Checking starting position ${counter + 1} of ${startingPositions.size}, current max = $max")
+        val energized = getEnergizedForStartingPosition(input, startingPosition)
+        if (energized > max)
+            max = energized
+    }
+    return max
+}
+
+private fun getEnergizedForStartingPosition(input: List<List<Char>>, startingPosition: Pair<Pair<Int, Int>, Direction>): Int {
     val isEnergized: MutableList<MutableList<Boolean>> = MutableList(input.size) { MutableList(input[0].size) { false } }
-    var currentPositions = setOf(Pair(Pair(0, 0), Direction.West))
+    var currentPositions = setOf(startingPosition)
     val lastPositions = mutableSetOf<Set<Pair<Pair<Int, Int>, Direction>>>()
 
     while (currentPositions.isNotEmpty() && (currentPositions !in lastPositions)) {
@@ -20,6 +37,19 @@ fun sixteenth(): Int {
     }
 
     return isEnergized.flatten().count { it }
+}
+
+private fun getStartingPositions(input: List<List<Char>>): Set<Pair<Pair<Int, Int>, Direction>> {
+    val startingPositions = mutableSetOf<Pair<Pair<Int, Int>, Direction>>()
+    for (line in input.indices) {
+        startingPositions.add(Pair(Pair(line, 0), Direction.West))
+        startingPositions.add(Pair(Pair(line, input[0].size - 1), Direction.East))
+    }
+    for (col in input[0].indices) {
+        startingPositions.add(Pair(Pair(0, col), Direction.North))
+        startingPositions.add(Pair(Pair(input.size - 1, col), Direction.South))
+    }
+    return startingPositions
 }
 
 private fun getNextPositions(
